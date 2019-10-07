cd16104cb14cf619bc6795191ae27cc7e61692ea
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-10-07 10:18:40 +0900

    [fix]getPattern修正

--- getPattern.c
+++ getPattern.c
@@ -1,6 +1,6 @@
-#include <header.h>
+#include "header.h"
 
-int main(void) {
+bool getPattern(short(*board)[BOARDSIZE]) {
 	int a, i, j = 0, Pattern[8] = { 0, 1, 2, 3, 4, 5, 6, 7 },
 		Pattern1[8][8] = {};
 	for (a = 0; a < 8; a++) {
