d16210186f3d5f17331484137f92093428c39b62
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-10-08 17:28:05 +0900

    [clean]使用していない変数を削除

--- setPattern.c
+++ setPattern.c
@@ -7,14 +7,13 @@ int memory_now = 0;
 short(*zipptr)[BOARDSIZE] = 0;
 
 bool setPattern(const short(*board)[BOARDSIZE]) {
-	static int memory_max, size_magni;
+	static int size_magni;
 
 	if (memory_now >= SIZE * size_magni) {//容量が足りてないなら
 		if (size_magni < MAGNI_MAX) {//容量の既定の最大値を超えないなら
 			//容量を増やす
 			size_magni++;
 			zipptr = (short(*)[BOARDSIZE])realloc((void*)zipptr, sizeof(short) * BOARDSIZE * SIZE * size_magni);
-			memory_max += SIZE;
 		}
 		else {
 			return false;
