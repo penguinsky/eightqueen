ede31b2a89d1ae9d4b1b75a0669aedd1a54fac02
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-10-11 12:12:44 +0900

    [add]calcPattern()実装

---
+++ calcBoard.c
@@ -0,0 +1,42 @@
+#include"header.h"
+
+void calcPattern(short(*board)[BOARDSIZE],int i,int j) 
+{
+	int k;
+
+	for (k = 0;k < BOARDSIZE;k++) 
+	{
+		board[k][j] += 1;//縦処理
+		board[i][k] += 1;//横処理
+	}
+
+	if (i > j)
+	{
+		for (k = 0;k < BOARDSIZE - (i - j);k++)
+		{
+			board[k + (i - j)][k] += 1;//右下斜め（i>jのとき）
+		}
+	}
+	else
+	{
+		for (k = 0;k < BOARDSIZE - (j - i);k++)
+		{
+			board[k][k + (j - i)] += 1;//右下斜め（i<=jのとき）
+		}
+	}
+
+	if (i + j < BOARDSIZE)
+	{
+		for (k = 0;k <= i + j;k++)
+		{
+			board[i + j - k][k] += 1;//左下斜め（i+J< BOARDSIZEのとき）
+		}
+	}
+	else
+	{
+		for (k = i + j - BOARDSIZE + 1;k < BOARDSIZE;k++)
+		{
+			board[i + j - k][k] += 1;//左下斜め（i+j>= BOARDSIZEのとき）
+		}
+	}
+}
--- header.h
+++ header.h
@@ -7,6 +7,6 @@ bool setPattern(const short(*board)[BOARDSIZE]);	//boardをパターンに圧縮して記録
 bool getPattern(short(*board)[BOARDSIZE]);			//記録したパターンからboardを再現する
 
 void initBoard(short(*board)[BOARDSIZE]);			//boardを初期化(0埋め)する
-void calcPattern(void);								//8クイーンを満たすパターンを全て計算し、setPattern()で記録する
+void calcPattern(short(*board)[BOARDSIZE], int i, int j);	//8クイーンを満たすパターンを全て計算し、setPattern()で記録する
 void draw(short(*board)[BOARDSIZE]);				//記録したパターン全てをdrawBoard()で描画する
 void drawBoard(const short(*board)[BOARDSIZE]);		//boardから盤面を描画する
