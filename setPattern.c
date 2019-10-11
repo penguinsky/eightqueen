be69c9e2aebf0abab75737bb910ef1b9f1ce8b03
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-10-11 12:11:14 +0900

    [clean]コメントを追加

--- setPattern.c
+++ setPattern.c
@@ -2,17 +2,17 @@
 #include"Pattern.h"
 #include<stdlib.h>
 
-#define SIZE 100
-#define MAGNI_MAX 5
+#define SIZE 100	//一度に確保する容量(short[BOARDSIZE]単位)
+#define MAGNI_MAX 5	//確保する容量の最大値
 int memory_now = 0;
 short(*zipptr)[BOARDSIZE] = NULL;
 
 bool setPattern(const short(*board)[BOARDSIZE]) {
 	static int size_magni;
 
-	if (memory_now >= SIZE * size_magni) {//容量が足りてないなら
-		if (size_magni < MAGNI_MAX) {//容量の既定の最大値を超えないなら
-			//容量を増やす
+	if (memory_now >= SIZE * size_magni) {	//容量が足りてないなら
+		if (size_magni < MAGNI_MAX) {		//容量の既定の最大値を超えないなら
+
 			int capacity = sizeof(short) * BOARDSIZE * SIZE * ++size_magni;
 			zipptr = (short(*)[BOARDSIZE])realloc((void*)zipptr, capacity);
 		}
